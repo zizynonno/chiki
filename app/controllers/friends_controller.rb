101003b5c8669454d24ef71b7ac2cd8fd907e905
zizynonno <zizynonno@gmail.com>
2019-12-12 00:39:42 +0900

    add bootstrap

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,13 +1,18 @@
 class FriendsController < ApplicationController
+  before_action :sign_in_required, only: [:new,:edit,:index,:show]
   def new
+    @friend = Friend.new
   end
 
   def edit
+    @friend = Friend.find(params[:id])
   end
 
   def index
+    @friend = Friend.all
   end
 
   def show
+    @friend = Friend.all
   end
 end
